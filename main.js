65179a1d11aaa8b98b2e5cd8e088e8762ec11014
lecfab <lecfab@gmail.com>
2017-04-03 10:12:08 +0200

    icon

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ const url = require('url')
 let mainWindow
 
 function createWindow() {
-    mainWindow = new BrowserWindow({ width: 1200, height: 600, icon: "images/icon.svg" })
+    mainWindow = new BrowserWindow({ width: 1200, height: 600, icon: "images/icon.png" })
     mainWindow.maximize()
 
     mainWindow.loadURL(url.format({
